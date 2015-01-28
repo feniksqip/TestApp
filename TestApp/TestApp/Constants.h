6e089caeff21bf1595877055e8687b6b339567a5
Admin <admin@Mac-Admin.local>
2015-01-28 12:58:14 +0300

    added

---
+++ TestApp/TestApp/Constants.h
@@ -0,0 +1,26 @@
+//
+//  Constants.h
+//  TestApp
+//
+//  Created by Admin on 28/01/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#ifndef TestApp_Constants_h
+#define TestApp_Constants_h
+
+
+// Google Analytics Information
+#define GA_APP_ID                                       @"UA-59024144-1"
+#define GA_APP_NAME                                     @"TestApp"
+#define GA_USER_NAME                                    @"feniksqip"
+
+
+// Parse.com Information
+#define PARSE.COM_APP_ID                                @"zjefEJuBMdkImxH8A9DQJzlBgTCZXW35vyzkkwbA"
+#define PARSE.COM_CLIENT_KEY                            @"drBIzTEUmnMGXsFnLokn24w5EPwlC94lhtfR0oE7"
+
+
+
+
+#endif

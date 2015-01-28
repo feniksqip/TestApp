c681d4110a76c7518455ed9f49c66e841e3606b5
Admin <admin@Mac-Admin.local>
2015-01-28 12:46:30 +0300

    added clean

---
+++ TestApp/TestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by Admin on 28/01/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

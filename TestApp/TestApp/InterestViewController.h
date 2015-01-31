688224c1b59930c0d8988c87e2ce58c2abe43877
Admin <admin@Mac-Admin.local>
2015-01-31 19:14:50 +0300

    add buttons

--- TestApp/TestApp/InterestViewController.h
+++ TestApp/TestApp/InterestViewController.h
@@ -9,5 +9,32 @@
 #import <UIKit/UIKit.h>
 
 @interface InterestViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *var1;
+@property (weak, nonatomic) IBOutlet UIButton *var2;
+@property (weak, nonatomic) IBOutlet UIButton *var3;
+- (IBAction)var1Action:(id)sender;
+- (IBAction)var2Action:(id)sender;
+- (IBAction)var3Action:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *msg1;
+@property (weak, nonatomic) IBOutlet UIButton *msg2;
+@property (weak, nonatomic) IBOutlet UIButton *msg3;
+@property (weak, nonatomic) IBOutlet UIButton *msg4;
+@property (weak, nonatomic) IBOutlet UIButton *msg5;
+- (IBAction)msg1Action:(id)sender;
+- (IBAction)msg2Action:(id)sender;
+- (IBAction)msg3Action:(id)sender;
+- (IBAction)msg4Action:(id)sender;
+- (IBAction)msg5Action:(id)sender;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *msgTextField;
+
+@property (nonatomic) NSNumber *buttonIndex;
+@property (nonatomic) NSNumber *messageIndex;
+
+
+- (IBAction)okBtnAction:(id)sender;
 
 @end
--- TestApp/TestApp/InterestViewController.m
+++ TestApp/TestApp/InterestViewController.m
@@ -14,6 +14,28 @@
 
 @implementation InterestViewController
 
+@synthesize buttonIndex;
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:YES];
+    
+    self.buttonIndex = [NSNumber numberWithInt:101];
+    self.messageIndex = [NSNumber numberWithInt:201];
+    
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    if ([defaults objectForKey:@"BUTTON_INDEX"] != nil) {
+        self.buttonIndex = [defaults objectForKey:@"BUTTON_INDEX"];
+    }
+    if ([defaults objectForKey:@"MESSAGE_INDEX"] != nil) {
+        self.messageIndex = [defaults objectForKey:@"MESSAGE_INDEX"];
+    }
+    
+    
+    
+    [self changeButtonStatus:self.buttonIndex];
+    [self changeMessageStatus:self.messageIndex];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -36,4 +58,106 @@
 - (IBAction)exitSegueForModal:(UIStoryboardSegue *)segue
 {
 }
+- (IBAction)var1Action:(id)sender {
+    [self changeButtonStatus:[NSNumber numberWithInt:101]];
+}
+
+- (IBAction)var2Action:(id)sender {
+    [self changeButtonStatus:[NSNumber numberWithInt:102]];
+}
+
+- (IBAction)var3Action:(id)sender {
+    [self changeButtonStatus:[NSNumber numberWithInt:103]];
+}
+
+- (void) changeButtonStatus:(id)sender {
+    NSNumber *select = (NSNumber*) sender;
+
+    if ([select isEqualToNumber:[NSNumber numberWithInt:101]]) {
+        buttonIndex = [NSNumber numberWithInt:101];
+        self.var1.backgroundColor = [UIColor orangeColor];
+        self.var2.backgroundColor = nil;
+        self.var3.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:102]]) {
+        buttonIndex = [NSNumber numberWithInt:102];
+        self.var2.backgroundColor = [UIColor greenColor];
+        self.var1.backgroundColor = nil;
+        self.var3.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:103]]) {
+        buttonIndex = [NSNumber numberWithInt:103];
+        self.var3.backgroundColor = [UIColor redColor];
+        self.var1.backgroundColor = nil;
+        self.var2.backgroundColor = nil;
+    }
+}
+- (void) changeMessageStatus:(id)sender {
+    NSNumber *select = (NSNumber*) sender;
+    
+    if ([select isEqualToNumber:[NSNumber numberWithInt:201]]) {
+        buttonIndex = [NSNumber numberWithInt:201];
+        self.msg1.backgroundColor = [UIColor grayColor];
+        self.msg2.backgroundColor = nil;
+        self.msg3.backgroundColor = nil;
+        self.msg4.backgroundColor = nil;
+        self.msg5.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:202]]) {
+        buttonIndex = [NSNumber numberWithInt:202];
+        self.msg2.backgroundColor = [UIColor grayColor];
+        self.msg1.backgroundColor = nil;
+        self.msg3.backgroundColor = nil;
+        self.msg4.backgroundColor = nil;
+        self.msg5.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:203]]) {
+        buttonIndex = [NSNumber numberWithInt:203];
+        self.msg3.backgroundColor = [UIColor grayColor];
+        self.msg1.backgroundColor = nil;
+        self.msg2.backgroundColor = nil;
+        self.msg4.backgroundColor = nil;
+        self.msg5.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:204]]) {
+        buttonIndex = [NSNumber numberWithInt:204];
+        self.msg4.backgroundColor = [UIColor grayColor];
+        self.msg1.backgroundColor = nil;
+        self.msg2.backgroundColor = nil;
+        self.msg3.backgroundColor = nil;
+        self.msg5.backgroundColor = nil;
+    } else if ([select isEqualToNumber:[NSNumber numberWithInt:205]]) {
+        buttonIndex = [NSNumber numberWithInt:205];
+        self.msg5.backgroundColor = [UIColor grayColor];
+        self.msg1.backgroundColor = nil;
+        self.msg2.backgroundColor = nil;
+        self.msg3.backgroundColor = nil;
+        self.msg4.backgroundColor = nil;
+    }
+    
+}
+- (IBAction)msg1Action:(id)sender {
+    [self changeMessageStatus:[NSNumber numberWithInt:201]];
+}
+
+- (IBAction)msg2Action:(id)sender {
+    [self changeMessageStatus:[NSNumber numberWithInt:202]];
+}
+
+- (IBAction)msg3Action:(id)sender {
+    [self changeMessageStatus:[NSNumber numberWithInt:203]];
+}
+
+- (IBAction)msg4Action:(id)sender {
+    [self changeMessageStatus:[NSNumber numberWithInt:204]];
+}
+
+- (IBAction)msg5Action:(id)sender {
+    [self changeMessageStatus:[NSNumber numberWithInt:205]];
+}
+- (IBAction)okBtnAction:(id)sender {
+
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setObject:self.buttonIndex forKey:@"BUTTON_INDEX"];
+    [defaults setObject:self.messageIndex forKey:@"MESSAGE_INDEX"];
+    [defaults synchronize];
+    
+    NSLog(@"--- Data saved to UserDefaults ---");
+}
+
 @end
